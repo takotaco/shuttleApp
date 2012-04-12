5348e39df2928fafb110157f84cceb9c395b87f0
Jim Z. Zhang <jzzhang@unix13.andrew.cmu.edu>
2012-04-12 02:42:01 -0400

    changed resize to 100

--- javascript/detectBrowser.js
+++ javascript/detectBrowser.js
@@ -3,8 +3,8 @@ function detectBrowser() {
     var mapdiv = document.getElementById("map_canvas");
 
     if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
-	mapdiv.style.width = '50%';
-	mapdiv.style.height = '50%';
+	mapdiv.style.width = '100%';
+	mapdiv.style.height = '100%';
     } else {
 	mapdiv.style.width = '800px';
 	mapdiv.style.height = '600px';
