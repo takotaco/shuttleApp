7e895b2dc0e9eced24693f6ec9a8c493f72a2755
Laine Herron <LaineHerron@gmail.com>
2012-04-11 22:55:35 -0400

    Added a bar at the top for imputing addresses, and added geocode.js to the imports.

--- javascript/initialize.js
+++ javascript/initialize.js
@@ -1,11 +1,17 @@
+var map;
+var geocoder;
 function initialize() {
     var myOptions = {
         center: new google.maps.LatLng(40.443504, -79.941571),
         zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
+    map = new google.maps.Map(document.getElementById("map_canvas"),
 				  myOptions);
+
     allKMLRoutes(map);
+    geocoder = new google.maps.Geocoder();
+
+ 
 }
 
