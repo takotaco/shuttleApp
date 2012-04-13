cc24879543ccbabb7e9d0129d6912e8d6fc53797
Laine Herron <LaineHerron@gmail.com>
2012-04-12 21:13:59 -0400

    added code to set start address from box

--- javascript/geocode.js
+++ javascript/geocode.js
@@ -13,3 +13,21 @@ function codeAddress() {
   });
  }
 
+function setStartLocation() {
+  alert("Called setStartLocation()");
+  var address = document.getElementById("startAddress").value + ",Pittsburgh, PA";
+  geocoder.geocode( { 'address': address}, function(results, status) {
+   if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      Currentmarker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location,
+        title:"Your are here"
+      });
+      mylat = results[0].geometry.location.lat();
+      mylong = results[0].geometry.location.lng();
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+ }
